724ddb89e6001858e24e2af7d796faa5dd0c5395
over <overln2@gmail.com>
2014-12-04 21:07:12 +0600

    Public API: make Tuple public

--- core/src/main/java/com/catinthedark/cw_inc/lib/util/Tuple.java
+++ core/src/main/java/com/catinthedark/cw_inc/lib/util/Tuple.java
@@ -3,11 +3,11 @@ package com.catinthedark.cw_inc.lib.util;
 /**
  * Created by over on 04.12.14.
  */
-class Tuple<A, B> {
+public class Tuple<A, B> {
     final A a;
     final B b;
 
-    Tuple(A a, B b) {
+    public Tuple(A a, B b) {
         this.a = a;
         this.b = b;
     }
