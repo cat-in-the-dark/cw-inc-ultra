bca19d4e64a483aec7e5d27a4e4192266cd987ee
over <overln2@gmail.com>
2014-12-05 12:20:58 +0600

    First shader!

--- core/src/main/java/com/catinthedark/cw_inc/impl/view/GameScreen.java
+++ core/src/main/java/com/catinthedark/cw_inc/impl/view/GameScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.CatmullRomSpline;
 import com.badlogic.gdx.math.Vector2;
@@ -29,10 +30,13 @@ public class GameScreen extends Screen<RenderShared> {
                   }
               }, new Layer<RenderShared>() {
                   final SpriteBatch batch = new SpriteBatch();
+                  final ShaderProgram grayscale = new ShaderProgram(Gdx.files.internal("first.vert").readString(),
+                          Gdx.files.internal("first.frag").readString());
 
                   @Override
                   public void render(RenderShared shared) {
                       batch.setProjectionMatrix(shared.camera.combined);
+                      batch.setShader(grayscale);
                       batch.begin();
                       shared.levelView.forEach(row -> {
                           for (LevelBlock block : row) {
